8a21017a6123ba13bcb6ef841736f4b22e1b70e5
Marco Hui <marco.hui@bluepin.hk>
2024-05-23 15:54:08 +0800

    upadte router

--- React/main.jsx
+++ React/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 // import "./index.css";
 
 const theme = createTheme({
@@ -15,7 +16,11 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/vote" element={<App />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   </React.StrictMode>
 );
