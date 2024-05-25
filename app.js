807114f2eb5be4f4d9fb5740cb998d7e4d824eb8
your_username <xuyuhaokaoka1@gmail.com>
2024-05-26 02:23:21 +0800

    qwd

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ const mongodb = require('mongodb');
 
 const cors = require("cors")
 
-app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}))
+app.use(cors())
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html')
 // })
