90d345aa66719d8ab8d7a0a5a01f4c47cd6eb380
Marco Hui <marco.hui@bluepin.hk>
2024-05-27 12:50:08 +0800

    qwd

--- app.js
+++ app.js
@@ -502,4 +502,5 @@ app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`Example app is running on ${process.env.NODE_ENV} mode`)
 })
